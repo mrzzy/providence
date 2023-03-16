c9f681787435581cf4968a6a0a294eca19ae34d5
Zhu Zhanyan <program.nom@gmail.com>
2023-03-16 03:58:07 +0000

    feat(simplygo): add SimplyGo.request() method to make a csrf-ed & authenticated request

--- sources/simpygo/src/main.rs
+++ sources/simpygo/src/main.rs
@@ -6,27 +6,24 @@
 use std::collections::HashMap;
 
 use reqwest::{
-    blocking::{Client, Response, RequestBuilder},
-    header::{HeaderMap, self}, Request,
+    blocking::{Client, Request, Response},
+    header::{HeaderMap, COOKIE},
+    Method,
 };
 use scraper::{Html, Selector};
 
 const SIMPLYGO_URL: &str = "https://simplygo.transitlink.com.sg";
 const CSRF_KEY: &str = "__RequestVerificationToken";
+const SESSION_ID_KEY: &str = "ASP.NET_SessionId";
+const AUTH_TOKEN_KEY: &str = "AuthToken";
 
 // Unit Tests
 #[cfg(test)]
 mod tests;
 
-/// Extract the value of the CSRF cookie from given SimplyGo homepage response headers
-fn extract_csrf_cookie(headers: &HeaderMap) -> &str {
-    // parse cookies from Set-Cookie http header
-    let cookies: HashMap<_, _> = headers
-        .get("Set-Cookie")
-        .expect("Missing 'Set-Cookie' header in Simplygo homepage response.")
-        .to_str()
-        .expect("Could not parse value of 'Set-Cookie' header.")
-        // split up multple cookies
+fn parse_cookies(cookies: &str) -> HashMap<&str, &str> {
+    // split up multple cookies
+    cookies
         .split(";")
         .map(|cookie| cookie.trim())
         // split cookie key & value
@@ -34,10 +31,20 @@ fn extract_csrf_cookie(headers: &HeaderMap) -> &str {
             let parts: Vec<_> = cookie.split("=").collect();
             parts.get(0).zip(parts.get(1)).map(|(&k, &v)| (k, v))
         })
-        .collect();
+        .collect()
+}
+
+/// Extract the value of the CSRF cookie from given SimplyGo homepage response headers
+fn extract_csrf_cookie(headers: &HeaderMap) -> &str {
+    // parse cookies from Set-Cookie http header
+    let cookies_text = headers
+        .get("Set-Cookie")
+        .expect("Missing 'Set-Cookie' header in Simplygo homepage response.")
+        .to_str()
+        .expect("Could not parse value of 'Set-Cookie' header.");
 
     // get value of csrf cookie
-    cookies
+    parse_cookies(cookies_text)
         .get(CSRF_KEY)
         .map(|&v| v)
         .expect("Expected cookie with CSRF token is missing.")
@@ -62,7 +69,7 @@ fn extract_csrf_form(html: &str) -> String {
 }
 
 /// CSRF Tokens to be submitted in requests to SimplyGo.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct CSRF {
     /// CSRF token to be submitted as a cookie.
     cookie: String,
@@ -71,7 +78,7 @@ struct CSRF {
 }
 impl CSRF {
     /// Derive CSRF by scraping give SimplyGo homepage resposne
-    fn from(homepage: &Response) -> Self {
+    fn from(homepage: Response) -> Self {
         Self {
             cookie: extract_csrf_cookie(&homepage.headers()).to_owned(),
             form: extract_csrf_form(
@@ -83,7 +90,6 @@ impl CSRF {
     }
 }
 
-
 // Defines an authenticated session on Simplygo
 struct Session {
     id: String,
@@ -91,27 +97,67 @@ struct Session {
 }
 
 /// SimplyGo client
-struct Client {
+struct SimplyGo {
     http: Client,
     csrf: CSRF,
     session: Option<Session>,
 }
-impl Client {
+impl SimplyGo {
     pub fn new() -> Self {
         let http = Client::new();
-        let csrf = CSRF::from(http
-            .get(SIMPLYGO_URL)
-            .send()
-            .expect("Failed to GET Simplygo homepage."));
+        let csrf = CSRF::from(
+            http.get(SIMPLYGO_URL)
+                .send()
+                .expect("Failed to GET Simplygo homepage."),
+        );
         Self {
             http,
             csrf,
             session: None,
         }
     }
+
+    // Make a HTTP request with the given method & URL path to SimplyGo
+    // Attaches CSRF & session tokens (if present) to the request.
+    // Returns the Response on success, Error on failure.
+    fn request(
+        self,
+        method: Method,
+        url_path: &str,
+        mut form_data: HashMap<String, String>,
+    ) -> Request {
+        // insert csrf token into form data
+        form_data.insert(CSRF_KEY.to_owned(), self.csrf.form);
+
+        let cookies = [
+            // pass csrf token as cookie
+            vec![(CSRF_KEY, self.csrf.cookie)],
+            // pass auth & session id as cookies
+            self.session.map_or(vec![], |session| {
+                vec![(AUTH_TOKEN_KEY, session.auth), (SESSION_ID_KEY, session.id)]
+            }),
+        ]
+        .concat();
+
+        self.http
+            .request(method, format!("{}{}", SIMPLYGO_URL, url_path))
+            // build cookies header by expressing cookies in the format:
+            // <KEY1>=<VALUE1>; <KEY2>=<VALUE2> ...
+            .header(
+                COOKIE,
+                cookies
+                    .into_iter()
+                    .map(|(k, v)| format!("{}={}", k, v))
+                    .collect::<Vec<_>>()
+                    .join("; "),
+            )
+            .form(&form_data)
+            .build()
+            .expect("Failed to build SimplyGo request.")
+    }
 }
 
 fn main() {
-    let simplygo = Client::new();
+    let simplygo = SimplyGo::new();
     println!("{:?}", simplygo.csrf);
 }
--- sources/simpygo/src/tests.rs
+++ sources/simpygo/src/tests.rs
@@ -4,13 +4,15 @@
  * Unit Tests
 */
 
-use std::fs::read_to_string;
+use std::{fs::read_to_string, str::from_utf8};
 
-use reqwest::header::SET_COOKIE;
+use reqwest::{blocking::Client, header::SET_COOKIE};
 
 use super::*;
 
 const CSRF_TOKEN: &str = "_CSRF-TOKEN";
+const SESSION_ID: &str = "_session_id";
+const AUTH_TOKEN: &str = "_auth_token";
 
 #[test]
 fn extract_csrf_cookie_test() {
@@ -35,4 +37,40 @@ fn extract_csrf_form_test() {
     ))
     .unwrap();
     assert_eq!(CSRF_TOKEN, extract_csrf_form(&html));
+}
+
+#[test]
+fn simlygo_request_test() {
+    let simplygo = SimplyGo {
+        http: Client::new(),
+        csrf: CSRF {
+            cookie: CSRF_TOKEN.to_owned(),
+            form: CSRF_TOKEN.to_owned(),
+        },
+        session: Some(Session {
+            id: SESSION_ID.to_owned(),
+            auth: AUTH_TOKEN.to_owned(),
+        }),
+    };
+    let request = simplygo.request(Method::GET, "/test", HashMap::new());
+
+    // parse cookies from 'Cookie' http header
+    let cookies = parse_cookies(request.headers().get("Cookie").unwrap().to_str().unwrap());
+    // parse url encoded form data in request body
+    let body = from_utf8(request.body().unwrap().as_bytes().unwrap()).unwrap();
+    let form_data: HashMap<_, _> = body
+        .split('&')
+        .map(|entry| entry.split('=').collect::<Vec<_>>())
+        .map(|key_value| (key_value[0], key_value[1]))
+        .collect();
+
+    // test: http method & url
+    assert_eq!(format!("{}/test", SIMPLYGO_URL), request.url().as_str());
+    assert_eq!(Method::GET, request.method());
+    // test: csrf tokens are attached in form data & cookies
+    assert_eq!(CSRF_TOKEN, form_data[CSRF_KEY]);
+    assert_eq!(CSRF_TOKEN, cookies[CSRF_KEY]);
+    // test: session id & auth token attached in cookies
+    assert_eq!(SESSION_ID, cookies[SESSION_ID_KEY]);
+    assert_eq!(AUTH_TOKEN, cookies[AUTH_TOKEN_KEY]);
 }
