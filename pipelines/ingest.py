5a17195e057940b2bede329d65015871da226708
Zhu Zhanyan <program.nom@gmail.com>
2023-04-01 16:50:53 +0800

    revert: s3fs sidecar approach

--- pipelines/ingest.py
+++ pipelines/ingest.py
@@ -16,11 +16,8 @@ from airflow.models.baseoperator import BaseOperator
 from pendulum import datetime
 from kubernetes.client import models as k8s
 
-S3FS_SIDECAR_TEMPLATE = path.join(path.dirname(__file__), "s3fs_sidecar.yaml")
-
 
 def ingest_simplygo(
-    s3_staging_bucket: str,
     k8s_labels: Dict[str, str],
     # TODO(mrzzy): change to latest on merge
     image_tag: str = "main",
@@ -30,40 +27,28 @@ def ingest_simplygo(
 
     Args:
         image_tag: SimplyGo source container image to use tag.
-        s3_staging_bucket: Name of S3 bucket used to store staged data.
         k8s_labels: K8s labels to add to K8s Pod created by the Airflow task.
     """
-    # Extract SimplyGo data with SimplyGo source & load into S3 via S3FS sidecar
+    # Extract SimplyGo data with SimplyGo source & load into S3
     connection = BaseHook.get_connection("providence_simplygo_src")
     extract_load_s3 = KubernetesPodOperator(
-        pod_template_file=S3FS_SIDECAR_TEMPLATE,
         task_id="ingest_simplygo",
         image=f"ghcr.io/mrzzy/providence-simplygo-src:{image_tag}",
         # TODO(mrzzy): remove on merge
-        image_pull_policy="always",
+        image_pull_policy="Always",
         labels=k8s_labels
         | {
             "app.kubernetes.io/name": "simpygo_src",
             "app.kubernetes.io/component": "source",
             "app.kubernetes.io/version": image_tag,
         },
-        params={
-            # tell s3fs sidecar which s3 bucket to mount
-            "s3_bucket": s3_staging_bucket
-        },
-        volume_mounts=[
-            k8s.V1VolumeMount(
-                name="s3-bucket",
-                mount_path="/mnt/s3fs",
-            )
-        ],
         arguments=[
             "--trips-from",
             "{{ data_interval_start | ds }}",
             "--trips-to",
             "{{ data_interval_end | ds }}",
             "--output",
-            "/mnt/s3fs/providence/raw/simplygo/date={{ ds }}/data.json",
+            "s3://{{ context.params.s3_bucket }}/providence/raw/simplygo/date={{ ds }}/data.json",
         ],
         env_vars=[
             k8s.V1EnvVar("SIMPLYGO_SRC_USERNAME", connection.login),
@@ -84,27 +69,26 @@ def ingest_simplygo(
 def ingest_data(
     s3_staging_bucket: str = "mrzzy-co-data-lake",
 ):
-    """
-        Providence Ingestion Data Pipeline.
-        Ingests data from Data Sources into AWS Redshift & uses AWS S3 as staging area.
-    fa
-        Parameters:
-        - `s3_staging_bucket`: Name of a existing S3 bucket to stage data.
-        - `k8s_labels` Labels to attach to all K8s pods created by this DAG.
+    """Providence Ingestion Data Pipeline.
+    Ingests data from Data Sources into AWS Redshift & uses AWS S3 as staging area.
 
-        Connections by expected id:
-        - `providence_simplygo_src":
-            - **Login** SimplyGo username.
-            - **Password** SimplyGo password.
-        - `aws_default`:
-            - **Login** AWS Access Key ID.
-            - **Password** AWS Access Secret Key.
+    Parameters:
+    - `s3_bucket`: Name of a existing S3 bucket to stage data.
+    - `k8s_labels` Labels to attach to all K8s pods created by this DAG.
+
+    Connections by expected id:
+    - `providence_simplygo_src":
+        - **Login** SimplyGo username.
+        - **Password** SimplyGo password.
+    - `aws_default`:
+        - **Login** AWS Access Key ID.
+        - **Password** AWS Access Secret Key.
     """
     k8s_labels = {
         "app.kubernetes.io/part-of": "providence",
         "app.kubernetes.io/managed-by": "airflow",
     }
-    ingest_simplygo(s3_staging_bucket, k8s_labels)
+    ingest_simplygo(k8s_labels)
 
 
 ingest_data()
