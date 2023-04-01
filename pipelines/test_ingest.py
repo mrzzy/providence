a74f218fb8684228de3beaf265b0b2337e1d5430
Zhu Zhanyan <program.nom@gmail.com>
2023-04-01 14:18:38 +0800

    test(pipelines): fix irreproducable test by pegging airflow dag dir

--- pipelines/test_ingest.py
+++ pipelines/test_ingest.py
@@ -4,9 +4,12 @@
 # Data Ingestion
 #
 
+from os.path import dirname
 import pytest
 from unittest import mock
 from airflow.models import Connection, DagBag
+
+DAGS_DIR = dirname(__file__)
 
 
 @pytest.mark.unit
@@ -26,7 +29,7 @@ def test_ingest_providence_data_import():
         "os.environ",
         **{f"AIRFLOW_CONN_{c.conn_id.upper()}": c.get_uri() for c in connections},  # type: ignore
     ):
-        dagbag = DagBag()
+        dagbag = DagBag(DAGS_DIR)
     assert dagbag.import_errors == {}
     expected_dags = ["ingest_providence_data"]
     for dag_id in expected_dags:
