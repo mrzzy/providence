88844d3a32df339c4c409a8adc4124952fd71141
Zhu Zhanyan <program.nom@gmail.com>
2023-04-03 07:10:22 +0800

    feat(pipelines): update ingest pipeline to output simplygo data straight to aws s3

--- pipelines/ingest.py
+++ pipelines/ingest.py
@@ -6,7 +6,7 @@
 
 from os import path
 from datetime import timedelta
-from typing import Dict
+from typing import Dict, List
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
@@ -17,23 +17,44 @@ from pendulum import datetime
 from kubernetes.client import models as k8s
 
 
+def get_aws_env(conn_id: str) -> Dict[str, str]:
+    """Get environment variables used to configure AWS access from the Airflow connection.
+
+    Args:
+        conn_id: ID of the Airflow connection to retrieve for aws credentials.
+    Returns:
+        Dict of environment variable name & value needed to configure AWS.
+    """
+    aws = BaseHook.get_connection(conn_id)
+    return {
+        "AWS_DEFAULT_REGION": aws.extra_dejson["region_name"],
+        "AWS_ACCESS_KEY_ID": str(aws.login),
+        "AWS_SECRET_ACCESS_KEY": aws.password,
+    }
+
+
+def k8s_env_vars(env_vars: Dict[str, str]) -> List[k8s.V1EnvVar]:
+    """Convert given env_vars into list of K8s env vars."""
+    return [k8s.V1EnvVar(name, value) for name, value in env_vars.items()]
+
+
 @dag(
     dag_id="ingest_providence_data",
     schedule=timedelta(days=1),
     start_date=datetime(2023, 2, 1, tz="utc"),
     catchup=False,
 )
-def ingest_data(s3_bucket: str = "mrzzy-co-data-lake", simplygo_src_tag: str = "main"):
+# TODO(mrzzy): change image tag back to main
+def ingest_data(
+    s3_bucket: str = "mrzzy-co-data-lake",
+    simplygo_src_tag: str = "feat-ingest-simplygo",
+):
     """Providence Ingestion Data Pipeline.
-        Ingests data from Data Sources into AWS Redshift & uses AWS S3 as staging area.
-    fa
-        Parameters:
-        - `s3_bucket`: Name of a existing S3 bucket to stage data.
-        - `simplygo_src_tag`: Tag specifying the version of the SimplyGo source container to use.
+    Ingests data from Data Sources into AWS Redshift & uses AWS S3 as staging area.
 
     Parameters:
     - `s3_bucket`: Name of a existing S3 bucket to stage data.
-    - `k8s_labels` Labels to attach to all K8s pods created by this DAG.
+    - `simplygo_src_tag`: Tag specifying the version of the SimplyGo source container to use.
 
     Connections by expected id:
     - `providence_simplygo_src":
@@ -49,36 +70,32 @@ def ingest_data(s3_bucket: str = "mrzzy-co-data-lake", simplygo_src_tag: str = "
     }
     # Extract SimplyGo data with SimplyGo source & load into S3
     simplygo_connection = BaseHook.get_connection("providence_simplygo_src")
-    extract_load_s3 = KubernetesPodOperator(
+    load_simplygo_s3 = KubernetesPodOperator(
         task_id="ingest_simplygo",
         image="ghcr.io/mrzzy/providence-simplygo-src:{{ params.simplygo_src_tag }}",
         # TODO(mrzzy): remove on merge
-        image_pull_policy="always",
+        image_pull_policy="Always",
         labels=k8s_labels
         | {
             "app.kubernetes.io/name": "simpygo_src",
             "app.kubernetes.io/component": "source",
             "app.kubernetes.io/version": "{{ params.simplygo_src_tag }}",
         },
-        # TODO: mount s3
-        volume_mounts=[
-            k8s.V1VolumeMount(
-                name="s3-bucket",
-                mount_path="/mnt/s3",
-            )
-        ],
         arguments=[
             "--trips-from",
             "{{ data_interval_start | ds }}",
             "--trips-to",
             "{{ data_interval_end | ds }}",
             "--output",
-            "/mnt/s3/providence/raw/simplygo/date={{ ds }}/data.json",
+            "s3://{{ params.s3_bucket }}/providence/raw/simplygo/{{ ds }}.json",
         ],
-        env_vars=[
-            k8s.V1EnvVar("SIMPLYGO_SRC_USERNAME", simplygo_connection.login),
-            k8s.V1EnvVar("SIMPLYGO_SRC_PASSWORD", simplygo_connection.password),
-        ],
+        env_vars=k8s_env_vars(
+            {
+                "SIMPLYGO_SRC_USERNAME": simplygo_connection.login,
+                "SIMPLYGO_SRC_PASSWORD": simplygo_connection.password,
+            }
+            | get_aws_env("aws_default")
+        ),
         # TODO(mrzzy): remove testing
         is_delete_operator_pod=False,
     )
