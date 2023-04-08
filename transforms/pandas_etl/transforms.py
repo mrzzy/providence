985492ddd5d3fb3139d5b2c09eb2dec3b9ba5d34
Zhu Zhanyan <program.nom@gmail.com>
2023-04-08 17:14:50 +0800

    feat(transactions): add extract_uob transform to extract transactions from uob export

--- transforms/pandas_etl/transforms.py
+++ transforms/pandas_etl/transforms.py
@@ -1,5 +1,4 @@
 #
-# Providence
 # Transforms
 # Transformations
 #
@@ -14,19 +13,14 @@ def promote_header(df: DataFrame) -> DataFrame:
     """Promote the first row as Dataframe Header"""
     df.columns = df.iloc[0]
     df = df[1:]
+    df.columns.name = None
     return df
 
 
 def extract_uob(df: DataFrame) -> DataFrame:
     """Extract UOB Bank transactions from the given excel transactions export."""
     # Extract metadata as transposed 3-5 rows from header section
-    meta_df = (
-        df.iloc[3:6, :2]
-        .T
-        # drop unused index
-        .reset_index()
-        .drop(columns=["index"])
-    )
+    meta_df = df.iloc[3:6, :2].T
     # strip trailing ':', adapt transposed headers as column headers
     meta_df.iloc[0] = meta_df.iloc[0].str.strip(":")
     meta_df = promote_header(meta_df)
@@ -34,6 +28,9 @@ def extract_uob(df: DataFrame) -> DataFrame:
     # Extract transactions section
     transactions_df = promote_header(df[6:])
     # broadcast metadata dataframe into transforms
-    transactions_df[meta_df.columns] = meta_df.iloc[0]
+    transactions_df[meta_df.columns[1:]] = meta_df.iloc[0, 1:]
+    # reset index based on transactions rows
+    transactions_df = transactions_df.reset_index(drop=True)
+    transactions_df.columns.name = None
 
     return transactions_df
