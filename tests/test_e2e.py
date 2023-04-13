fb6a5db658bdfc4fda7dde751198c5b2e690bad2
Zhu Zhanyan <program.nom@gmail.com>
2023-04-13 20:58:16 +0800

    fix(e2e): timezone misaligned dates by generate date suffix with utc timezone
    as airflow interally uses utc

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -3,7 +3,7 @@
 # End to End Tests
 #
 
-from datetime import date
+from datetime import datetime
 import os
 import json
 from pathlib import Path
@@ -24,7 +24,9 @@ def s3_bucket():
     # upload test uob export into test bucket
     s3 = boto3.client("s3")
     # dag finds uob exports by date so we include today's date in the s3 key
-    key = date.today().strftime("providence/manual/uob/ACC_TXN_History_%d%m%Ytest.xls")
+    key = datetime.utcnow().date.strftime(
+        "providence/manual/uob/ACC_TXN_History_%d%m%Ytest.xls"
+    )
     s3.upload_file(str(RESOURCE_DIR / "ACC_TXN_test.xls"), test_bucket, key)
 
     yield test_bucket
