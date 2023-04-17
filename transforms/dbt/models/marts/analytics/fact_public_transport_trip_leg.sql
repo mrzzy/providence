a1c1cd98867a6557512992858d93061c8ed2bcec
Zhu Zhanyan <program.nom@gmail.com>
2023-04-17 12:39:51 +0800

    chore(transform): apply sqlfluff linting fixes

--- transforms/dbt/models/marts/analytics/fact_public_transport_trip_leg.sql
+++ transforms/dbt/models/marts/analytics/fact_public_transport_trip_leg.sql
@@ -3,22 +3,27 @@
 -- Transforms
 -- DBT Analytics: Public Transport Trip Legs Fact table
 --
-
 -- grain: 1 row = 1 trip leg
-with legs_merged_travel_on as (
-    select
-        {{ dbt_utils.star(
-            ref("stg_simplygo_trip_leg"),
-            except=["traveled_on", "begin_at"],
-        ) }},
-        -- merged in travel timestamp in utc timezone
-        convert_timezone('SGT', 'UTC', traveled_on + begin_at) as traveled_on
-    from {{ ref("stg_simplygo_trip_leg") }}
-)
+with
+    legs_merged_travel_on as (
+        select
+            {{
+                dbt_utils.star(
+                    ref("stg_simplygo_trip_leg"),
+                    except=["traveled_on", "begin_at"],
+                )
+            }},
+            -- merged in travel timestamp in utc timezone
+            convert_timezone('SGT', 'UTC', traveled_on + begin_at) as traveled_on
+        from {{ ref("stg_simplygo_trip_leg") }}
+    )
+
 select
     -- since bank cards cannot make concurrent trips, the combination
     -- of travel timestamp & card_id should be unique for each trip leg
-    {{ dbt_utils.generate_surrogate_key(["card_id", "traveled_on"]) }} as id,
+    {{ dbt_utils.generate_surrogate_key(["card_id", "traveled_on"]) }} as "id",
+    -- since bank cards cannot make concurrent trips, the combination
+    -- of travel timestamp & card_id should be unique for each trip leg
     date_trunc('day', traveled_on) as travel_date_id,
     traveled_on,
     posting_ref,
--- transforms/dbt/models/staging/simplygo/stg_simplygo_trip_leg.sql
+++ transforms/dbt/models/staging/simplygo/stg_simplygo_trip_leg.sql
@@ -8,9 +8,9 @@ select
     cast(t.traveled_on as date) as traveled_on,
     cast(l.begin_at as time) as begin_at,
     cast(l.cost_sgd as decimal(4, 2)) as cost_sgd,
-    cast(l.source as varchar) as source,
+    cast(l.source as varchar) as "source",
     cast(l.destination as varchar) as destination,
-    cast(l."mode" as varchar) as transport_mode,
+    cast(l.mode as varchar) as transport_mode,
     cast(t.card_id as varchar) as card_id,
     cast(s.scraped_on as timestamp) as scraped_on
 from {{ source("simplygo", "source_simplygo") }} as s, s.trips as t, t.legs as l
