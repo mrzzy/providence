4ae71f30b9fd06e1298be544d5b5ed7f169ab39d
Zhu Zhanyan <program.nom@gmail.com>
2023-04-17 16:31:06 +0800

    refactor(transform): remove redundundant cte in fact_public_transport_trip_leg sql

--- transforms/dbt/models/marts/analytics/fact_public_transport_trip_leg.sql
+++ transforms/dbt/models/marts/analytics/fact_public_transport_trip_leg.sql
@@ -4,33 +4,21 @@
 -- DBT Analytics: Public Transport Trip Legs Fact table
 --
 -- grain: 1 row = 1 trip leg
-with
-    legs_merged_travel_on as (
-        select
-            {{
-                dbt_utils.star(
-                    ref("stg_simplygo_trip_leg"),
-                    except=["traveled_on", "begin_at"],
-                )
-            }},
-            -- merged in travel timestamp in utc timezone
-            convert_timezone('SGT', 'UTC', traveled_on + begin_at) as traveled_on
-        from {{ ref("stg_simplygo_trip_leg") }}
-    )
-
 select
     -- since bank cards cannot make concurrent trips, the combination
     -- of travel timestamp & card_id should be unique for each trip leg
-    {{ dbt_utils.generate_surrogate_key(["card_id", "traveled_on"]) }} as "id",
-    -- since bank cards cannot make concurrent trips, the combination
-    -- of travel timestamp & card_id should be unique for each trip leg
-    date_trunc('day', traveled_on) as travel_date_id,
-    traveled_on,
-    posting_ref,
-    cost_sgd,
-    source,
-    destination,
-    transport_mode,
-    card_id as bank_card_id,
-    scraped_on as updated_at
-from legs_merged_travel_on
+    {{ dbt_utils.generate_surrogate_key(["card_id", "traveled_on", "begin_at"]) }}
+    as "id",
+    {{
+        dbt_utils.star(
+            ref("stg_simplygo_trip_leg"),
+            except=["traveled_on", "begin_at", "card_id"],
+        )
+    }},
+    -- merged in travel timestamp in utc timezone
+    convert_timezone('SGT', 'UTC', traveled_on + begin_at) as traveled_on,
+    scraped_on as updated_at,
+    -- foreign keys to dimensions
+    traveled_on as travel_date_id,
+    card_id as bank_card_id
+from {{ ref("stg_simplygo_trip_leg") }}
