bbf51cac22e4fa40185f775d8f2345ca298c7476
Zhu Zhanyan <program.nom@gmail.com>
2023-04-19 19:16:27 +0800

    fix(transforms): stg_map_ynab_uob_account dbt staging model mappings not unique

--- transforms/dbt/models/staging/native/stg_map_ynab_uob_account.sql
+++ transforms/dbt/models/staging/native/stg_map_ynab_uob_account.sql
@@ -3,7 +3,7 @@
 -- Transforms
 -- DBT Staging: YNAB UOB Account Mapping
 --
-select
+select distinct
     cast(budget_account_id as varchar) as budget_account_id,
     cast(vendor_id as varchar) as account_no
 from {{ source("mapping", "map_account") }}
