90ced052c9d54959c9998d1249328fb3bdc6fe9e
Zhu Zhanyan <program.nom@gmail.com>
2023-04-19 17:23:17 +0800

    feat(transforms): add dbt staging model stg_map_ynab_uob_account

---
+++ transforms/dbt/models/staging/native/stg_map_ynab_uob_account.sql
@@ -0,0 +1,10 @@
+--
+-- Providence
+-- Transforms
+-- DBT Staging: YNAB UOB Account Mapping
+--
+select
+    cast(budget_account_id as varchar) as budget_account_id,
+    cast(vendor_id as varchar) as account_no
+from {{ source("mapping", "map_account") }}
+where vendor = 'UOB'
