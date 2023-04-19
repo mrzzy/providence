dcd2595c09a99948d3f785c25ce97b560e09561c
Zhu Zhanyan <program.nom@gmail.com>
2023-04-19 19:29:22 +0800

    refactor(transforms): generalise stg_map_ynab_uob_account to stg_map_budget_account
    The latter is no longer vendor specific.

---
+++ transforms/dbt/models/staging/native/stg_map_budget_account.sql
@@ -0,0 +1,11 @@
+--
+-- Providence
+-- Transforms
+-- DBT Staging: YNAB Budget Account Mapping
+--
+select distinct
+    {{ dbt_utils.generate_surrogate_key(["budget_account_id", "vendor", "vendor_id"]) }} as id,
+    cast(budget_account_id as varchar) as budget_account_id,
+    cast(vendor as varchar) as vendor,
+    cast(vendor_id as varchar) as vendor_id
+from {{ source("mapping", "map_account") }}
