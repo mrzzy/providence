96ac1a98ef97528022b11d4767d9643389fac70b
Zhu Zhanyan <program.nom@gmail.com>
2023-04-20 06:32:46 +0800

    feat(transforms): add statement_begin & statement_end fields to stg_uob_account dbt model

--- transforms/dbt/models/staging/spectrum/uob/stg_uob_account.sql
+++ transforms/dbt/models/staging/spectrum/uob/stg_uob_account.sql
@@ -8,4 +8,7 @@ select distinct
   cast("account number" as varchar) as account_no,
   cast("account type" as varchar) as name,
   cast(currency as varchar) as currency_code
+  -- split_part() is 1-indexed
+  cast(split_part(statement_period, ' To ', 1) as date) as statement_begin,
+  cast(split_part(statement_period, ' To ', 2) as date) as statement_end
 from {{ source("uob", "source_uob") }}
