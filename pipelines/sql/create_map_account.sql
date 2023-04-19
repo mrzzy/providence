721f3889eab4b6e7fa571ac1a0f8a8d769f0f202
Zhu Zhanyan <program.nom@gmail.com>
2023-04-19 12:46:15 +0800

    fix(pipelines): create table not using redshift_schema dag param
    this defaults the table to be created in the default 'public' schema.

--- pipelines/sql/create_map_account.sql
+++ pipelines/sql/create_map_account.sql
@@ -3,7 +3,7 @@
 -- Pipelines
 -- map_account DDL
 --
-create table if not exists {{ params.redshift_table }} (
+create table if not exists {{ params.redshift_schema }}.{{ params.redshift_table }} (
   budget_account_id VARCHAR,
   vendor VARCHAR,
   vendor_account_id VARCHAR
