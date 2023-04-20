6046bb4eb9d20d3668b08253b056b292d3af236d
Zhu Zhanyan <program.nom@gmail.com>
2023-04-21 07:10:18 +0800

    perf(transforms): pushdown dedup predicate before inner join on dim_budget_category dbt model

--- transforms/dbt/models/marts/analytics/dim_budget_category.sql
+++ transforms/dbt/models/marts/analytics/dim_budget_category.sql
@@ -4,33 +4,32 @@
 -- DBT Analytics: Budget Category dimension
 --
 with
-    flat_categories as (
+    unique_categories as (
         select
-            {{ dbt_utils.generate_surrogate_key(["c.id", "c.budget_month"]) }} as "id",
             {{
                 dbt_utils.star(
                     ref("stg_ynab_budget_category"),
                     except=["id", "scraped_on"],
-                    relation_alias="c",
                 )
             }},
-            c.id as ynab_id,
-            c.scraped_on as updated_at,
-            g.name as category_group
-        from {{ ref("stg_ynab_budget_category") }} as c
-        inner join
-            {{ ref("stg_ynab_budget_category_group") }} as g
-            on g.id = c.category_group_id
+            id as category_id,
+            scraped_on as updated_at
+        from
+            (
+                {{
+                    dbt_utils.deduplicate(
+                        relation=ref("stg_ynab_budget_category"),
+                        partition_by="id",
+                        order_by="scraped_on desc",
+                    )
+                }}
+            )
     )
 
-select *
-from
-    (
-        {{
-            dbt_utils.deduplicate(
-                relation="flat_categories",
-                partition_by="id",
-                order_by="updated_at desc",
-            )
-        }}
-    )
+select
+    {{ dbt_utils.generate_surrogate_key(["c.category_id", "c.budget_month"]) }} as id,
+    c.*,
+    g.name as category_group
+from unique_categories c
+inner join
+    {{ ref("stg_ynab_budget_category_group") }} as g on g.id = c.category_group_id
