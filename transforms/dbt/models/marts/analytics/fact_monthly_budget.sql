c3b33e095be066ecd4f384db15c1545bc772626e
Zhu Zhanyan <program.nom@gmail.com>
2023-04-21 14:56:04 +0800

    fix(transforms): deduplicate fact_monthly_budget & fact_public_transport_trip_leg

--- transforms/dbt/models/marts/analytics/fact_monthly_budget.sql
+++ transforms/dbt/models/marts/analytics/fact_monthly_budget.sql
@@ -4,15 +4,12 @@
 -- DBT Analytics: Monthly budget Facts
 --
 -- grain 1 row: 1 monthly budget snapshot
+
 select
-    {{ dbt_utils.generate_surrogate_key(["c.budget_month", "b.id", "c.id"]) }} as "id",
-    c.budget_month as month_date_id,
-    b.id as budget_id,
-    c.id as category_id,
-    c.budget_amount as amount,
-    c.goal_type as goal_type,
-    c.goal_amount as goal_amount,
-    c.goal_due as goal_due,
-    c.updated_at as updated_at
-from {{ ref("dim_budget_category") }} as c
-inner join {{ ref("dim_budget") }} as b on b.id = c.budget_id
+    "id",
+    budget_month as month_date_id,
+    budget_id,
+    category_id,
+    budget_amount as amount,
+    updated_at
+from {{ ref("int_unique_budget_category") }} as u
--- transforms/dbt/models/marts/analytics/fact_public_transport_trip_leg.sql
+++ transforms/dbt/models/marts/analytics/fact_public_transport_trip_leg.sql
@@ -4,23 +4,41 @@
 -- DBT Analytics: Public Transport Trip Legs Facts
 --
 -- grain: 1 row = 1 trip leg snapshot
-select
-    -- since bank cards cannot make concurrent trips, the combination
-    -- of travel timestamp & card_id should be unique for each trip leg
-    {{ dbt_utils.generate_surrogate_key(["card_id", "traveled_on", "begin_at"]) }}
-    as "id",
-    {{
-        dbt_utils.star(
-            ref("stg_simplygo_trip_leg"),
-            except=["traveled_on", "begin_at", "card_id", "posting_ref"],
-        )
-    }},
-    posting_ref as billing_ref,
-    posting_ref is null as is_billed,
-    -- merged in travel timestamp in utc timezone
-    convert_timezone('SGT', 'UTC', traveled_on + begin_at) as traveled_on,
-    scraped_on as updated_at,
-    -- foreign keys to dimensions
-    traveled_on as travel_date_id,
-    card_id as bank_card_id
-from {{ ref("stg_simplygo_trip_leg") }}
+with
+    trip_legs_duplicated as (
+        select
+            -- since bank cards cannot make concurrent trips, the combination
+            -- of travel timestamp & card_id should be unique for each trip leg
+            {{
+                dbt_utils.generate_surrogate_key(
+                    ["card_id", "traveled_on", "begin_at"]
+                )
+            }} as "id",
+            {{
+                dbt_utils.star(
+                    ref("stg_simplygo_trip_leg"),
+                    except=["traveled_on", "begin_at", "card_id", "posting_ref"],
+                )
+            }},
+            posting_ref as billing_ref,
+            posting_ref is null as is_billed,
+            -- merged in travel timestamp in utc timezone
+            convert_timezone('SGT', 'UTC', traveled_on + begin_at) as traveled_on,
+            scraped_on as updated_at,
+            -- foreign keys to dimensions
+            traveled_on as travel_date_id,
+            card_id as bank_card_id
+        from {{ ref("stg_simplygo_trip_leg") }}
+    )
+
+select *
+from
+    (
+        {{
+            deduplicate(
+                relation="trip_legs_duplicated",
+                partition_by="id",
+                order_by="updated_at desc",
+            )
+        }}
+    )
