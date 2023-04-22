f0c6aebfc4ee633ee64b5d50914517b0a9b1b784
Zhu Zhanyan <program.nom@gmail.com>
2023-04-22 11:26:13 +0800

    refactor(transform): rename 'modified_on' budget field to 'modified_at'

--- transforms/dbt/models/staging/spectrum/ynab/stg_ynab_budget.sql
+++ transforms/dbt/models/staging/spectrum/ynab/stg_ynab_budget.sql
@@ -6,7 +6,7 @@
 select
     cast(id as varchar) as "id",
     cast(name as varchar) as "name",
-    cast(last_modified_on as timestamp) as modified_on,
+    cast(last_modified_on as timestamp) as modified_at,
     cast(currency_format.iso_code as varchar) as currency_code,
     cast(currency_format.currency_symbol as varchar) as currency_symbol,
     cast(_ynab_src_scraped_on as timestamp) as scraped_on
