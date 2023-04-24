22b6020ace5e0c9d0bde8ecef77ae51c0d92322c
Zhu Zhanyan <program.nom@gmail.com>
2023-04-25 07:17:55 +0800

    feat(transforms): allow user to specify n row column name in deduplicate() dbt macro

--- transforms/dbt/macros/deduplicate.sql
+++ transforms/dbt/macros/deduplicate.sql
@@ -6,7 +6,7 @@
 -- Deduplicate the given relation by partitioning, ordering & picking the first row.
 -- Custom implementation of deduplicate until the one in dbt-utils is fixed
 -- https://github.com/dbt-labs/dbt-utils/issues/713
-{% macro deduplicate(relation, partition_by, order_by) -%}
+{% macro deduplicate(relation, partition_by, order_by, n_row_col="n_row") -%}
 select *
 from
     (
@@ -14,8 +14,8 @@ from
             *,
             row_number() over (
                 partition by {{ partition_by }} order by {{ order_by }}
-            ) as _n_row
+            ) as {{ n_row_col }}
         from {{ relation }}
     )
-where _n_row = 1
+where {{ n_row_col }} = 1
 {%- endmacro %}
