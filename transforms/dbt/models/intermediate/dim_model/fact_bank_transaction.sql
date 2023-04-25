d15d378e5d66c1b6a6ae624042ea22b0efb071ea
Zhu Zhanyan <program.nom@gmail.com>
2023-04-25 09:37:45 +0800

    feat(transforms): add initial balance transaction to fact_bank_transaction dbt model
    Facilates aggregation of amounts in the fact table to equate to account balance.

--- transforms/dbt/models/intermediate/dim_model/fact_bank_transaction.sql
+++ transforms/dbt/models/intermediate/dim_model/fact_bank_transaction.sql
@@ -24,15 +24,52 @@ with
                 )
             }}
         )
+    ),
+
+    -- enrich transactions with account dimension
+    enriched_transactions as (
+        select t.*, a.id as account_id
+        from unique_transactions as t
+        left join {{ ref("dim_account") }} as a on a.vendor_id = t.account_no
+    ),
+
+    -- insert initial balance transaction so that sum of transaction amount will
+    -- tally with account balance
+    initial_balances as (
+        select
+            account_id,
+            statement_begin,
+            balance as balance,
+            processed_on
+        from ({{
+            deduplicate(
+                relation="enriched_transactions",
+                partition_by="account_id",
+                order_by="processed_on asc",
+                n_row_col="_n_row_account",
+            )
+        }})
+    ),
+
+initial_transaction as (
+        select
+            {{ dbt_utils.generate_surrogate_key(["account_id"]) }} as "id",
+            statement_begin as date_id,
+            account_id,
+            'Initial Balance' as description,
+            processed_on as updated_at,
+            balance as amount
+        from initial_balances
     )
 
 -- grain: 1 row = 1 bank transaction
+select * from initial_transaction
+union all
 select
-    t.id,
-    t.transacted_on as date_id,
-    a.id as account_id,
-    t.description,
-    t.processed_on as updated_at,
-    t.deposit - t.withdrawal as amount
-from unique_transactions as t
-left join {{ ref("dim_account") }} as a on a.vendor_id = t.account_no
+    id,
+    transacted_on as date_id,
+    account_id,
+    description,
+    processed_on as updated_at,
+    deposit - withdrawal as amount
+from enriched_transactions
