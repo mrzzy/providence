4b9106ca81fe5dff8b54ebc9d965d1338775ee30
Zhu Zhanyan <program.nom@gmail.com>
2023-04-26 06:48:56 +0800

    feat(transforms): add 'spending' & 'income' fields to mart_finance_dashboard

--- transforms/dbt/models/marts/mart_finance_dashboard.sql
+++ transforms/dbt/models/marts/mart_finance_dashboard.sql
@@ -4,8 +4,20 @@
 -- DBT Marts: Finance Dashboard
 --
 select
-  t.amount as transaction_amount,
-  t.date_id as transaction_date,
-  a.is_cash as account_is_cash
+    -- transaction info
+    t.amount as transaction_amount,
+    t.date_id as transaction_date,
+    a.is_cash as account_is_cash,
+    -- income / spending
+    (
+        case
+            when t.amount < 0 and t.transfer_account_id is null then t.amount else 0
+        end
+    ) as spending,
+    (
+        case
+            when t.amount > 0 and t.transfer_account_id is null then t.amount else 0
+        end
+    ) as income
 from {{ ref("fact_accounting_transaction") }} as t
-left join {{ ref("dim_account") }} as a.id = t.account_id
+left join {{ ref("dim_account") }} as a on a.id = t.account_id
