e0fc7fc817be6ff7e33212a7a8f0a7c0334fc81f
Zhu Zhanyan <program.nom@gmail.com>
2023-04-26 08:47:30 +0800

    refactor(pipelines): disable auto catchup for all airflow dags

--- pipelines/common.py
+++ pipelines/common.py
@@ -29,6 +29,7 @@ SQL_DIR = str(Path(__file__).parent / "sql")
 DAG_ARGS = {
     "catchup": False,
     "tags": ["providence"],
+    "catchup": False,
     # common args passed to all tasks
     "default_args": {
         # retry with exponential backoff on task failure
