11c8484da91e3e1e2f874c1da69dbf4148184e31
Zhu Zhanyan <program.nom@gmail.com>
2023-04-26 14:38:10 +0800

    refactor(transforms): mart_finance_dashboard to use is_passive, is_unaccounted flags from ynab_payee_flag seed
    Squashed commit of the following:
    
    commit 17509453fa18f06eef28bab54e757204faf8b6aa
    Author: Zhu Zhanyan <program.nom@gmail.com>
    Date:   Wed Apr 26 14:32:19 2023 +0800
    
        refactor(transforms): mart_finance_dashboard to use is_passive, is_unaccounted flags from dim_payee
    
    commit 52afe1c107adcf4f64d267733f7f46f490ee7d4b
    Author: Zhu Zhanyan <program.nom@gmail.com>
    Date:   Wed Apr 26 14:31:49 2023 +0800
    
        feat(transforms): add 'is_unaccounted' & 'is_passive' flags to dim_payee
    
    commit 0e24b0278e2cb23a88f8b04ac077f10ba9ed7537
    Author: Zhu Zhanyan <program.nom@gmail.com>
    Date:   Wed Apr 26 14:03:44 2023 +0800
    
        feat(transforms): add ynab_payee_flag dbt seed to associate payee with flags

--- transforms/dbt/models/intermediate/dim_model/dim_payee.sql
+++ transforms/dbt/models/intermediate/dim_model/dim_payee.sql
@@ -3,16 +3,20 @@
 -- Transforms
 -- DBT Intermediate: Payee Dimension
 --
+
+with unique_payees as  (
+    {{
+        deduplicate(
+            relation=ref("stg_ynab_payee"),
+            partition_by="id",
+            order_by="scraped_on desc",
+        )
+    }}
+)
 select
-    {{ dbt_utils.star(ref("stg_ynab_payee"), except=["scraped_on"]) }},
-    scraped_on as updated_at
-from
-    (
-        {{
-            deduplicate(
-                relation=ref("stg_ynab_payee"),
-                partition_by="id",
-                order_by="scraped_on desc",
-            )
-        }}
-    )
+    {{ dbt_utils.star(ref("stg_ynab_payee"), except=["scraped_on"], relation_alias="p") }},
+    scraped_on as updated_at,
+    coalesce(f.is_unaccounted, false) as is_unaccounted,
+    coalesce(f.is_passive, false) as is_passive
+from unique_payees p
+left join {{ ref("ynab_payee_flag") }} f on f.payee_id = p.id
--- transforms/dbt/models/marts/mart_finance_dashboard.sql
+++ transforms/dbt/models/marts/mart_finance_dashboard.sql
@@ -12,39 +12,20 @@ select
     t.date_id as transaction_date,
     t.description as transaction_description,
     a.is_cash as account_is_cash,
-    -- TODO(mrzzy): pushdown is_passive, is_unaccounted to fact_accounting_transaction
     -- spending
     (
         case
             when t.amount < 0 and t.transfer_account_id is null and a.is_cash then t.amount else 0
         end
     ) as spending,
-    -- unaccounted Spending: any spending associated with the adjustment payee
-    coalesce(
-        p.id in (
-            '10339a7c-b54b-46a8-9141-a793d5bdfb1a',  -- Manual Balance Adjustment
-            '11e89a59-2f9c-4b76-9af9-3baf57eb7a18'  -- Reconciliation Balance Adjustment
-        ),
-        -- consider no assigned payee as unaccounted spending
-        true
-    ) as is_unaccounted,
+    coalesce(p.is_unaccounted, false) as is_unaccounted,
     -- income
     (
         case
             when t.amount > 0 and t.transfer_account_id is null and a.is_cash then t.amount else 0
         end
     ) as income,
-    -- passive income: any income derived from the following financial institution
-    coalesce(
-        p.id in (
-            '3304a058-a21e-4dd6-8545-1f19102f8f9a',  -- Standard Charted Bank
-            '376c5870-56ca-46f5-b1f2-7a2ebf5dadf3',  -- UOB Bank
-            '05eae682-21f0-4fb9-8dcb-da72963fa5d5',  -- SG Government: CPF
-            '99d50734-caa5-4638-a0bc-d8941a7984ff'  -- Central Depository
-        ),
-        -- default to assigning non passive categorisation if no payee is attached.
-        false
-    ) as is_passive
+    coalesce(p.is_passive, false) as is_passive
 from {{ ref("fact_accounting_transaction") }} as t
 left join {{ ref("dim_account") }} as a on a.id = t.account_id
 left join {{ ref("dim_date") }} as d on d.id = t.date_id
