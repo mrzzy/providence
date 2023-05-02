accc7ac84b580d85663f25bce747560aff479e16
Zhu Zhanyan <program.nom@gmail.com>
2023-05-02 11:48:13 +0800

    feat(transforms): add stg_map_bank_card dbt model to expose bank card mapping

---
+++ transforms/dbt/models/staging/native/stg_map_bank_card.sql
@@ -0,0 +1,12 @@
+--
+-- Providence
+-- Transforms
+-- DBT Staging: Bank Card-Account Mapping
+--
+
+select
+    {{ dbt_utils.generate_surrogate_key(["bank_card_id", "vendor", "vendor_id"]) }} as "id",
+    cast(bank_card_id as varchar) as bank_card_id,
+    cast(vendor as varchar) as vendor,
+    cast(vendor_id as varchar) as vendor_id
+from {{ source("mapping", "map_bank_card") }}
--- transforms/dbt/models/staging/native/stg_map_budget_account.sql
+++ transforms/dbt/models/staging/native/stg_map_budget_account.sql
@@ -3,7 +3,7 @@
 -- Transforms
 -- DBT Staging: YNAB Budget Account Mapping
 --
-select distinct
+select
     {{ dbt_utils.generate_surrogate_key(["budget_account_id", "vendor", "vendor_id"]) }}
     as "id",
     cast(budget_account_id as varchar) as budget_account_id,
