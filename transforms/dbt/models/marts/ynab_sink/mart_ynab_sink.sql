1312e3f73c360112eb94f1c3b1cdc61dc3087646
Zhu Zhanyan <program.nom@gmail.com>
2023-05-05 14:54:39 +0800

    feat(transforms): add import_id to mart_ynab_sink dbt model to identify imports

--- transforms/dbt/models/marts/ynab_sink/mart_ynab_sink.sql
+++ transforms/dbt/models/marts/ynab_sink/mart_ynab_sink.sql
@@ -5,9 +5,12 @@
 --
 -- public transport trips yet to be accounted for.
 select
+    'ynab_sink:' || t.id as import_id,
+    'public_transport:' || t.billing_ref as subtransaction_group_id,
     t.account_id,
     t.updated_at as "date",
-    t.cost_sgd as amount,
+    -- ynab expresses amounts in milliunits: 1000 milliunits = $1
+    t.cost_sgd * 1000 as amount,
     '0b849f31-3e30-4a00-8b49-053a8365133f' as payee_id,
     -- SG Gov: Land Transport Authority
     '8efad1f9-ef85-45e4-a85e-74f0207dc2c1' as category_id,
@@ -15,9 +18,7 @@ select
     t.billing_ref as memo,
     'uncleared' as cleared,
     true as approved,
-    null as flag_color,
-    null as import_id,
-    'public_transport_' || t.billing_ref as subtransaction_group_id
+    null as flag_color
 from {{ ref("fact_public_transport_trip_leg") }} as t
 where
     t.is_billed
