ee67a2b4a376e189d6847580be670bb65ebca7c3
Zhu Zhanyan <program.nom@gmail.com>
2023-05-12 08:01:39 +0800

    test(ynab-sink): fix node-postgres import error in tests by switching ts-node to output esm mods

--- sinks/ynab/jest.config.js
+++ sinks/ynab/jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
   // fix '*.js' imports
