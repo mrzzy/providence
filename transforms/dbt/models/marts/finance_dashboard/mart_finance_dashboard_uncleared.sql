0dfb7bd84cb150af869c0d4f42d4e50c543bfe93
Zhu Zhanyan <program.nom@gmail.com>
2023-05-12 13:01:29 +0800

    feat(transform-dbt): matching uncleared transactions should ensure same account matches

--- transforms/dbt/models/marts/finance_dashboard/mart_finance_dashboard_uncleared.sql
+++ transforms/dbt/models/marts/finance_dashboard/mart_finance_dashboard_uncleared.sql
@@ -7,14 +7,14 @@ with
     -- uncleared accounting transactions
     unreconciled_accounting as (
         -- full transactions
-        select id, date_id, description, amount
+        select id, date_id, account_id, description, amount
         from {{ ref("fact_accounting_transaction") }}
         where clearing_status = 'uncleared' and super_id is null
         union all
         -- split transactions composed of multiple subtransactions
         -- each split transaction will be 1 transaction on the vendor account side
         -- group subtransactions into split transactions better matching by amount.
-        select t.id, t."date" as date_id, t.description, sum(t.amount)
+        select t.id, t."date" as date_id, account_id, t.description, sum(t.amount)
         from {{ ref("int_unique_transaction") }} as t
         inner join
             (
@@ -22,13 +22,13 @@ with
             ) as s
             on s.super_id = t.id
         where clearing_status = 'uncleared'
-        group by t.id, t."date", t.description
+        group by t.id, t."date", t.account_id, t.description
     ),
 
     -- unreconciled vendor transactions:
     -- any transaction since account was last reconciled.
     unreconciled_vendor as (
-        select t.id, t.date_id, t.description, t.amount
+        select t.id, t.date_id, t.account_id, t.description, t.amount
         from {{ ref("fact_vendor_transaction") }} as t
         left join {{ ref("dim_account") }} as a on a.id = t.account_id
         where t.date_id > a.last_reconciled_at
@@ -42,4 +42,5 @@ select
     (case when v.id is not null then '✔️' else '❌' end) as in_vendor
 -- join possible matching transactions by amount
 from unreconciled_accounting as a
-full join unreconciled_vendor as v on a.amount = v.amount
+full join
+    unreconciled_vendor as v on a.account_id = v.account_id and a.amount = v.amount
