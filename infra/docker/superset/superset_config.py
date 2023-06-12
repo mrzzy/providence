92c983d0e3ebb55e6ae8e4507e79460b42238754
Zhu Zhanyan <program.nom@gmail.com>
2023-06-12 14:42:06 +0800

    feat(infra): add standalone superset to docker-compose for local dev

---
+++ infra/docker/superset/superset_config.py
@@ -0,0 +1,21 @@
+#
+# Providence
+# Superset Container for Docker Compose Deploymeent
+# Superset Config
+#
+
+# Flask App Builder configuration
+# Your App secret key will be used for securely signing the session cookie
+# and encrypting sensitive information on the database
+# Make sure you are changing this key for your deployment with a strong key.
+# Alternatively you can set it with `SUPERSET_SECRET_KEY` environment variable.
+# You MUST set this for production environments or the server will not refuse
+# to start and you will see an error in the logs accordingly.
+SECRET_KEY = "YOUR_OWN_RANDOM_GENERATED_SECRET_KEY"
+
+# The SQLAlchemy connection string to your database backend
+# This connection defines the path to the database that stores your
+# superset metadata (slices, connections, tables, dashboards, ...).
+# Note that the connection information to connect to the datasources
+# you want to explore are managed directly in the web UI
+SQLALCHEMY_DATABASE_URI = "sqlite:////data/superset.db"
