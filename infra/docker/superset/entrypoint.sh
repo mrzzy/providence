b8aeb898865b267e2e3b626d38398afd66f1b9dc
Zhu Zhanyan <program.nom@gmail.com>
2023-06-12 18:03:53 +0800

    refactor(infra): harden superset container by specifying custom password

---
+++ infra/docker/superset/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Providence
+# Superset Container for Docker Compose Deploymeent
+# Entrypoint Script
+#
+
+set -ex -o pipefail
+
+# migrate db
+superset db upgrade
+# create admin user, disabling shell echo to avoid echoing password
+set +x
+echo "Creating Admin User..."
+superset fab create-admin \
+  --username admin \
+  --firstname Superset \
+  --lastname Admin \
+  --email admin@superset.com \
+  --password $ADMIN_PASSWORD
+set -x
+
+# Setting up roSetting up roles and permsles and perms
+superset init
+
+/usr/bin/run-server.sh
