3020709cfe00702883a52f08d0927c816b8bf15e
Zhu Zhanyan <program.nom@gmail.com>
2023-06-21 21:45:49 +0800

    refactor(simplygo-src): RCloneSink to use 'rclone rcat' instead of 'rclone copyto'
    The former avoids redundant copy of the scraped file on disk.

--- sources/simplygo/src/rclone.rs
+++ sources/simplygo/src/rclone.rs
@@ -4,19 +4,15 @@
  * RClone Sink
 */
 
-use std::io::{Error, ErrorKind, Write};
-use std::process::Command;
-
-use tempfile::NamedTempFile;
+use std::io::Write;
+use std::process::{Child, ChildStdin, Command, Stdio};
 
 /// Sink that writes written to an RClone location specified by target_path.
 /// Expects rclone binary to be accessible in PATH
-/// Note that writes written to a local temporary file until flush() is called
-/// to commit writes to Rclone Remote.
+/// Note that once write is committed with `flush()`, the sink is considered
+/// closed and can no longer be writtened to.
 pub struct RCloneSink {
-    target_path: String,
-    /// Temporary file to store writes before they are committed.
-    buffer: NamedTempFile,
+    rclone: Child,
 }
 impl RCloneSink {
     /// Create a new Rclone Sink that writes to given target_path.
@@ -24,71 +20,54 @@ impl RCloneSink {
     /// <RCLONE_REMOTE>:<REMOTE_PATH>
     pub fn new(target_path: &str) -> Self {
         Self {
-            target_path: target_path.to_string(),
-            buffer: NamedTempFile::new().unwrap_or_else(|e| {
-                panic!(
-                    "Unexpected error creating temporary file for writing: {}",
-                    e
-                )
-            }),
+            // launch rclone rcat process that writes stdin to target path
+            rclone: Command::new("rclone")
+                .stdin(Stdio::piped())
+                .args(["rcat", target_path])
+                .spawn()
+                .unwrap_or_else(|e| panic!("Could not run 'rclone rcat': {}", e)),
         }
     }
 
-    fn buffer_path(&self) -> &str {
-        self.buffer
-            .path()
-            .to_str()
-            .expect("Unable to convert name of temporary file to string")
+    fn stdin(&mut self) -> &mut ChildStdin {
+        self.rclone
+            .stdin
+            .as_mut()
+            .expect("Could not obtain 'rclone rcat' stdin for writing")
     }
 }
 impl Write for RCloneSink {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.buffer.write(buf)
+        self.stdin().write(buf)
     }
+
     fn flush(&mut self) -> std::io::Result<()> {
-        // ensure any buffered writes are synced to file
-        self.buffer.flush()?;
-        // copy written file to rclone remote
-        let result = match Command::new("rclone")
-            .args(["copyto", "-vvv", &self.buffer_path(), &self.target_path])
-            .output()
-        {
-            // successful
-            Ok(out) if out.status.success() => Ok(()),
-            // command ran but with non zero exit
-            Ok(out) => Err(Error::new(
-                ErrorKind::Other,
-                format!(
-                    "rclone copy to failed: {}",
-                    std::str::from_utf8(&out.stderr).unwrap_or_else(|e| panic!(
-                        "Could not decode non utf-8 output from rclone: {}",
-                        e
-                    ))
-                ),
-            )),
-            // command could not run
-            Err(e) => Err(Error::new(ErrorKind::Other, e)),
-        };
+        let result = self.stdin().flush();
+        // take and drop stdin stream to close it, causing rclone to terminate
+        self.rclone
+            .stdin
+            .take()
+            .expect("Could not obtain 'rclone rcat' stdin for closing");
+        // wait for rclone to terminate
+        self.rclone
+            .wait()
+            .expect("Could not wait for 'rclone rcat' as it is not running");
         result
     }
 }
+
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
+
+    use tempfile::NamedTempFile;
 
     use super::*;
 
     #[test]
-    fn new_test() {
-        let target_path = ":s3,key='value':/remote/path";
-        let sink = RCloneSink::new(target_path);
-        assert!(Path::new(&sink.buffer_path()).exists());
-    }
-
-    #[test]
-    fn write_test() {
-        let target_path = ":s3,key='value':/remote/path";
-        let mut sink = RCloneSink::new(target_path);
+    fn sink_test() {
+        let target = NamedTempFile::new()
+            .expect("Could not create temporary file: {}");
+        let mut sink = RCloneSink::new(&target.path().to_string_lossy());
         sink.write(b"test").expect("Failed to write");
     }
 }
