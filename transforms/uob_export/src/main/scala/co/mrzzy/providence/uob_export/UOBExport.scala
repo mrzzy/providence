fcc2848153287f6c8ac041ea57427de004dc9d15
Zhu Zhanyan <program.nom@gmail.com>
2023-06-25 19:21:36 +0800

    feat(uob-export-tfm): implement uob export transform with spark

--- transforms/uob_export/src/main/scala/co/mrzzy/providence/uob_export/UOBExport.scala
+++ transforms/uob_export/src/main/scala/co/mrzzy/providence/uob_export/UOBExport.scala
@@ -7,22 +7,47 @@
 package co.mrzzy.providence.uob_export
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.DecimalType
+import org.apache.spark.sql.types.TimestampType
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.types.DateType
+import org.apache.spark.sql.functions
 
 object UOBExport {
   val SparkExcelFormat = "com.crealytics.spark.excel";
+  val TimestampCol = "__uob_export_timestamp"
 
   /** Schema of Bank transactions read from UOB exports */
   val BankTransaction = StructType(
     Seq(
-      StructField("Transaction Date", StringType),
-      StructField("Transaction Description", StringType),
+      StructField("TransactionDate", DateType),
+      StructField("TransactionDescription", StringType),
       StructField("Withdrawal", DecimalType(13, 2)),
       StructField("Deposit", DecimalType(13, 2)),
-      StructField("Available Balance", DecimalType(13, 2))
+      StructField("AvailableBalance", DecimalType(13, 2))
+    )
+  )
+
+  /** Schema of metadata read from UOB exports */
+  val Metadata = StructType(
+    Seq(
+      StructField("AccountNumber", StringType),
+      StructField("AccountType", StringType),
+      StructField("StatementPeriod", StringType),
+      StructField("Currency", StringType)
+    )
+  )
+
+  /** Schema of the Bank export output */
+  val BankExport = StructType(
+    BankTransaction ++ Metadata :+ StructField(
+      TimestampCol,
+      TimestampType,
+      nullable = false
     )
   )
 
@@ -49,7 +74,73 @@ object UOBExport {
       .load(path)
   }
 
-  def readMeta = ???
+  /** Extract the metadata from rows 5-7 of the UOB excel export into a
+    * Dataframe.
+    *
+    * @param path
+    *   Path to UOB bank transaction excel export.
+    * @param spark
+    *   Spark Session used to interface with spark.
+    * @return
+    *   Extracted metadata as a DataFrame.
+    */
+  def readMeta(
+      path: String
+  )(implicit
+      spark: SparkSession
+  ): DataFrame = {
+    import spark.implicits._
+    // extract metadata from rows 5-7 of the export
+    val metaRows = spark.read
+      .format(SparkExcelFormat)
+      .option("header", true)
+      .option("dataAddress", "A5:C8")
+      .load(path)
+      .collect()
+    val metadata = Row(
+      metaRows.map(_.getString(1)) :+
+        // currency is oddly placed, so we extract it manually
+        metaRows(0).getString(2): _*
+    )
+    spark.createDataFrame(
+      spark.sparkContext.parallelize(Seq(metadata)),
+      Metadata
+    )
+  }
+
+  /** Extract the UOB Excel Export into a Tabular Bank Export DataFrame
+    *
+    * @param path
+    *   Path to UOB bank transaction excel export.
+    * @param spark
+    *   Spark Session used to interface with spark.
+    * @return
+    *   Tabular Bank Export DataFrame.
+    */
+  def read(path: String)(implicit spark: SparkSession): DataFrame = {
+    // join metadata to every transaction
+    readTransactions(path)
+      .crossJoin(readMeta(path))
+      // add processing timestamp
+      .withColumn(TimestampCol, functions.current_timestamp())
+  }
+
+  def write(df: DataFrame, path: String) {
+    df.write
+      .format("delta")
+      .mode(SaveMode.Overwrite)
+      .partitionBy("TransactionDate")
+      .save(path)
+  }
+
+  def configDelta(builder: SparkSession.Builder): SparkSession.Builder = {
+    builder
+      .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+      .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog"
+      )
+  }
 
   def main(args: Array[String]): Unit = {
     // parse command args
@@ -64,7 +155,12 @@ at path 'export_xlsx' and write them into a Delta Lake table at path 'output_del
       sys.exit(1)
     }
     val (exportXlsx, outputDelta) = (args(0), args(1))
-    implicit val spark =
-      SparkSession.builder.appName("pvd-uob-export").getOrCreate()
+    implicit val spark = configDelta(
+      SparkSession.builder
+        .appName("pvd-uob-export")
+    ).getOrCreate()
+
+    // write extracted bank export into delta table.
+    write(read(exportXlsx), outputDelta)
   }
 }
--- transforms/uob_export/src/test/scala/co/mrzzy/providence/uob_export/UOBExportSpec.scala
+++ transforms/uob_export/src/test/scala/co/mrzzy/providence/uob_export/UOBExportSpec.scala
@@ -6,19 +6,48 @@
 
 package co.mrzzy.providence.uob_export
 
+import scala.reflect.io.Directory
 import org.apache.spark.sql.SparkSession
+import java.nio.file.Files
 
 class UOBExportSpec extends munit.FunSuite {
-  implicit val spark = SparkSession.builder
-    .appName("pvd-uob-export")
-    .master("local[*]")
+  implicit val spark = UOBExport
+    .configDelta(
+      SparkSession.builder
+        .appName("pvd-uob-export")
+        .master("local[*]")
+    )
     .getOrCreate()
 
   test("readTransactions() reads bank transactions from UOB export") {
     val df = UOBExport
       .readTransactions(getClass().getResource("/ACC_TXN_test.xls").getPath())
     // test: dimensions of export dataframe
-    assertEquals(df.columns.length, 5)
+    assertEquals(df.schema, UOBExport.BankTransaction)
     assertEquals(df.count(), 2L)
+  }
+
+  test("readMetadata() reads metadata from UOB export") {
+    val df = UOBExport
+      .readMeta(getClass().getResource("/ACC_TXN_test.xls").getPath())
+    assertEquals(df.schema, UOBExport.Metadata)
+    assertEquals(df.count(), 1L)
+  }
+
+  lazy val df = UOBExport
+    .read(getClass().getResource("/ACC_TXN_test.xls").getPath())
+
+  test("read() joins transactions & metadata, adds timestamp column") {
+    assertEquals(df.schema, UOBExport.BankExport)
+    assertEquals(df.count(), 2L)
+
+    assert(df.columns.contains(UOBExport.TimestampCol))
+  }
+
+  test("write() writes dataframe delta paritioned by date") {
+    val targetPath = Files.createTempDirectory("UOBExportSpec_write")
+    UOBExport.write(df, targetPath.toString)
+    spark.read.format("delta").load(targetPath.toUri.toString)
+    new Directory(targetPath.toFile).deleteRecursively
   }
 }
