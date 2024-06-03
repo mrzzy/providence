715198377669f25f70ccb9bf2906ec7939d77cb1
Zhu Zhanyan <program.nom@gmail.com>
2024-06-03 11:29:23 +0800

    fix(pipelines): typo in pipeline image name

--- pipelines/deploy.py
+++ pipelines/deploy.py
@@ -16,7 +16,7 @@ async def deploy_pipelines(work_pool: str = "azure-container-instances"):
     await deploy(
         await ingest_simplygo.to_deployment(name="pvd-ingest-simplygo"),
         work_pool_name=work_pool,
-        image="ghcr.io/mrzzy/providence/pvd-pipeline:latest",
+        image="ghcr.io/mrzzy/pvd-pipeline:latest",
         build=False,
     )
 
