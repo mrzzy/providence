03977644fc3fa2ad426c1a9844a957e37cf2183c
Zhu Zhanyan <program.nom@gmail.com>
2024-06-03 11:11:42 +0800

    infra(pipelines): add simplygo_src & simplygo_tfm binaries required by simplygo pipeline

--- pipelines/test_simplygo.py
+++ pipelines/test_simplygo.py
@@ -12,6 +12,7 @@ from simplygo import ingest_simplygo
 from prefect import flow
 from prefect.testing.utilities import prefect_test_harness
 
+
 @pytest.mark.asyncio
 async def test_ingest_simplygo(prefect):
     await ingest_simplygo(date(2024, 5, 1))
