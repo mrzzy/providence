0753656cd1d7c0ddb92233b7ecf1fd9e516e9cb1
Zhu Zhanyan <program.nom@gmail.com>
2024-06-05 16:06:52 +0800

    test(pipelines): use S3Bucket block defined in prefect cloud
    hack to workaround https://www.github.com/PrefectHQ/prefect/issues/13349

--- pipelines/test_flows.py
+++ pipelines/test_flows.py
@@ -5,7 +5,9 @@
 
 # Usage: Set the following environment variables:
 # - B2_ACCOUNT_ID: B2 account id.
-# - B2_APP_KEY: B2 application key credentials
+# - B2_APP_KEY: B2 application key credentials.
+# - SIMPLYGO_SRC_USERNAME: Username used to authenticate with SimplyGo.
+# - SIMPLYGO_SRC_PASSWORD: Password used to authenticate with SimplyGo.
 # - PVD_LAKE_BUCKET: Name of the B2 bucket to use as data lake.
 # - YNAB_BUDGET_ID: Id of the budget to retrieve from YNAB API.
 
@@ -17,6 +19,7 @@ from prefect import flow
 from prefect.blocks.system import Secret
 from prefect.testing.utilities import prefect_test_harness
 from prefect_aws import AwsClientParameters, AwsCredentials, S3Bucket
+from pydantic import SecretStr
 
 from simplygo import ingest_simplygo
 from ynab import ingest_ynab
@@ -26,16 +29,18 @@ from ynab import ingest_ynab
 def prefect():
     with prefect_test_harness():
         # setup credential blocks
-        S3Bucket(
-            bucket_name=os.environ["PVD_LAKE_BUCKET"],
-            credentials=AwsCredentials(
-                aws_access_key_id=os.environ["B2_ACCOUNT_ID"],
-                aws_secret_access_key=os.environ["B2_APP_KEY"],
-                aws_client_parameters=AwsClientParameters(
-                    endpoint_url="s3.us-west-004.backblazeb2.com",
-                ),
-            ),
-        ).save("pvd-data-lake", overwrite=True)
+        # NOTE: S3 Bucket block not created due to due to https://github.com/PrefectHQ/prefect/issues/13349
+        # TODO(mrzzy): revert once issue has been resolved
+        # S3Bucket(
+        #     bucket_name=os.environ["PVD_LAKE_BUCKET"],
+        #     credentials=AwsCredentials(
+        #         aws_access_key_id=os.environ["B2_ACCOUNT_ID"],
+        #         aws_secret_access_key=SecretStr(os.environ["B2_APP_KEY"]),
+        #         aws_client_parameters=AwsClientParameters(
+        #             endpoint_url="s3.us-west-004.backblazeb2.com",
+        #         ),
+        #     ),
+        # ).save("pvd-data-lake", overwrite=True)
 
         Secret(value=os.environ["SIMPLYGO_SRC_USERNAME"]).save(
             "simplygo-src-username", overwrite=True
