947b4e312d7f473200f1485f4636883e969e9cb5
Zhu Zhanyan <program.nom@gmail.com>
2024-06-05 10:48:49 +0800

    test: merge flow integration code into test_flows.py

--- pipelines/test_flows.py
+++ pipelines/test_flows.py
@@ -7,13 +7,19 @@
 # - B2_ACCOUNT_ID: B2 account id.
 # - B2_APP_KEY: B2 application key credentials
 # - PVD_LAKE_BUCKET: Name of the B2 bucket to use as data lake.
-
+# - YNAB_BUDGET_ID: Id of the budget to 
 
 import os
-from prefect.blocks.system import Secret
+from datetime import date
+
 import pytest
+from prefect import flow
+from prefect.blocks.system import Secret
 from prefect.testing.utilities import prefect_test_harness
 from prefect_aws import AwsClientParameters, AwsCredentials, S3Bucket
+
+from simplygo import ingest_simplygo
+from ynab import ingest_ynab
 
 
 @pytest.fixture(scope="session")
@@ -37,5 +43,18 @@ def prefect():
         Secret(value=os.environ["SIMPLYGO_SRC_PASSWORD"]).save(
             "simplygo-src-password", overwrite=True
         )
+        Secret(value=os.environ["YNAB_ACCESS_TOKEN"]).save(
+            "ynab-access-token", overwrite=True
+        )
 
         yield
+
+
+@pytest.mark.asyncio
+async def test_ingest_simplygo(prefect):
+    await ingest_simplygo(date(2024, 5, 1))
+
+
+@pytest.mark.asyncio
+async def test_ingest_ynab(prefect):
+    await ingest_ynab(os.environ["YNAB_BUDGET_ID"])
