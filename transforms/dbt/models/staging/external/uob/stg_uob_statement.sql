cce8f21c3a3ff105716f840638ffddd00170ff9d
Zhu Zhanyan <program.nom@gmail.com>
2024-06-17 11:13:07 +0800

    feat(dbt-tfm): add scraped_on date column to stg_uob_statement model

--- transforms/dbt/models/staging/external/uob/stg_uob_statement.sql
+++ transforms/dbt/models/staging/external/uob/stg_uob_statement.sql
@@ -5,20 +5,21 @@
 --
 {% set date_fmt = "%d %b %Y" %}
 select
-    strptime("transaction date", '{{ date_fmt }}') as transacted_on,  -- noqa: RF05
+    strptime(s."transaction date", '{{ date_fmt }}') as transacted_on,  -- noqa: RF05
     -- replace newlines in transaction description with spaces
     replace(
-        cast("transaction description" as varchar),  -- noqa: RF05
+        cast(s."transaction description" as varchar),  -- noqa: RF05
         '\n',
         ' '
     ) as description,
-    cast(withdrawal as decimal(10, 2)) as withdrawal,
-    cast(deposit as decimal(10, 2)) as deposit,
-    cast("available balance" as decimal(10, 2)) as balance,  -- noqa: RF05
-    cast("account number" as varchar) as account_no,  -- noqa: RF05
-    cast("account type" as varchar) as "name",  -- noqa: RF05
-    cast(currency as varchar) as currency_code,
+    cast(s.withdrawal as decimal(10, 2)) as withdrawal,
+    cast(s.deposit as decimal(10, 2)) as deposit,
+    cast(s."available balance" as decimal(10, 2)) as balance,  -- noqa: RF05
+    cast(s."account number" as varchar) as account_no,  -- noqa: RF05
+    cast(s."account type" as varchar) as "name",  -- noqa: RF05
+    cast(s.currency as varchar) as currency_code,
     -- split_part() is 1-indexed
-    strptime(split_part("statement period", ' To ', 1), '{{ date_fmt }}')  as statement_begin, -- noqa: RF05
-    strptime(split_part("statement period", ' To ', 2), '{{ date_fmt }}') as statement_end -- noqa: RF05
-from {{ source("uob", "uob") }}k
+    strptime(split_part(s."statement period", ' To ', 1), '{{ date_fmt }}')  as statement_begin, -- noqa: RF05
+    strptime(split_part(s."statement period", ' To ', 2), '{{ date_fmt }}') as statement_end, -- noqa: RF05
+    {{ scraped_on("s") }} as scraped_on
+from {{ source("uob", "uob") }} s
