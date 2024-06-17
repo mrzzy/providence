d43efb7891296e80bf116d679ae66e5511041f79
Zhu Zhanyan <program.nom@gmail.com>
2024-06-17 12:13:25 +0800

    feat(dbt-tfm): port fact_public_transport_trip_leg to duckdb

--- transforms/dbt/models/intermediate/dim_model/fact_public_transport_trip_leg.sql
+++ transforms/dbt/models/intermediate/dim_model/fact_public_transport_trip_leg.sql
@@ -7,26 +7,20 @@
 with
     trip_legs_duplicated as (
         select
-            -- since bank cards cannot make concurrent trips, the combination
-            -- of travel timestamp & card_id should be unique for each trip leg
-            {{
-                dbt_utils.generate_surrogate_key(
-                    ["card_id", "traveled_on", "begin_at"]
-                )
-            }} as "id",
+            trip_id as id,
             {{
                 dbt_utils.star(
                     ref("stg_simplygo_trip_leg"),
-                    except=["traveled_on", "begin_at", "card_id", "posting_ref"],
+                    except=["traveled_on", "card_id", "posting_ref"],
                 )
             }},
             posting_ref as billing_ref,
             posting_ref is not null as is_billed,
-            -- merged in travel timestamp in utc timezone
-            convert_timezone('SGT', 'UTC', traveled_on + begin_at) as traveled_on,
+            -- convert travel timestamp in sgt (+08) to utc timezone
+            cast(traveled_on || '+08' as timestamptz) as traveled_on,
             scraped_on as updated_at,
             -- foreign keys to dimensions
-            traveled_on as travel_date_id,
+            date_trunc('day', traveled_on) as travel_date_id,
             card_id as bank_card_id
         from {{ ref("stg_simplygo_trip_leg") }}
     ),
