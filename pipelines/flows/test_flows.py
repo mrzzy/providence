8f1a721fc95b9686f919274f70ed57008ab48af0
Zhu Zhanyan <program.nom@gmail.com>
2024-06-19 10:42:10 +0800

    test(pipelines): remove concurrency from test_flows() as it overloads sqlite used in test

--- pipelines/flows/test_flows.py
+++ pipelines/flows/test_flows.py
@@ -13,12 +13,10 @@ from flows import ingest_simplygo, ingest_uob, ingest_ynab, transform_dbt
 
 @pytest.mark.asyncio
 async def test_flows(prefect):
-    await asyncio.gather(
-        ingest_simplygo(os.environ["PVD_LAKE_BUCKET"], date(2024, 5, 1)),
-        ingest_ynab(os.environ["PVD_LAKE_BUCKET"], os.environ["YNAB_BUDGET_ID"]),
-        ingest_uob(
-            bucket=os.environ["PVD_LAKE_BUCKET"],
-            export_path="raw/by=mrzzy/ACC_TXN_History_05062024124137.xls",
-        )
+    await ingest_simplygo(os.environ["PVD_LAKE_BUCKET"], date(2024, 5, 1))
+    await ingest_ynab(os.environ["PVD_LAKE_BUCKET"], os.environ["YNAB_BUDGET_ID"])
+    await ingest_uob(
+        bucket=os.environ["PVD_LAKE_BUCKET"],
+        export_path="raw/by=mrzzy/ACC_TXN_History_05062024124137.xls",
     )
     await transform_dbt()
