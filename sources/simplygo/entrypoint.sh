d389a7a1b8cbaf29194a67fef55818da0b7cbd12
Zhu Zhanyan <program.nom@gmail.com>
2024-11-14 15:12:07 +0800

    refactor(infra): update dockerfile & entrypoint.sh to support new python simplygo source

--- sources/simplygo/entrypoint.sh
+++ sources/simplygo/entrypoint.sh
@@ -4,9 +4,9 @@
 # Container Entrypoint
 #
 
-set -exuo pipefail
+set -exu -o pipefail
 
 # scrape SimplyGo with SimplyGo source
-simplygo_src --trips-from $1 --trips-to $1 --output-dir /tmp/out
+python simplygo_src.py --trips-from $1 --trips-to $1 --output /tmp/out.json
 # copy scraped data to B2
-rclone copy --size-only --fast-list /tmp/out :b2:mrzzy-co-data-lake/staging/by=simplygo_src/date=$1
+rclone copy --size-only --fast-list /tmp/out :b2:mrzzy-co-data-lake/staging/by=simplygo_src_v2/date=$1
